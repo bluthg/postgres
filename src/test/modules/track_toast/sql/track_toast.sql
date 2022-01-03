df4cc0d8318cf6726afe8d1ae36ef67a5cf04aca
Gunnar "Nick" Bluth <gunnar.bluth@pro-open.de>
2022-01-03 14:32:21 +0100

    adds a basic set of tests

---
+++ src/test/modules/track_toast/sql/track_toast.sql
@@ -0,0 +1,20 @@
+SHOW track_toast;
+TABLE pg_stat_toast; -- view exists
+CREATE TABLE toast_test (cola TEXT, colb TEXT COMPRESSION lz4, colc TEXT , cold TEXT, cole TEXT);
+ALTER TABLE toast_test ALTER colc SET STORAGE EXTERNAL;
+ALTER TABLE toast_test ALTER cold SET STORAGE MAIN;
+ALTER TABLE toast_test ALTER cole SET STORAGE PLAIN;
+INSERT INTO toast_test VALUES (repeat(md5('a'),100), repeat(md5('a'),100), repeat(md5('a'),100), repeat(md5('a'),100), repeat(md5('a'),100) );
+SELECT pg_sleep(1); -- give the stats collector some time to send the stats upstream
+SELECT attname
+	,storagemethod
+	,externalized
+	,compressmethod
+	,compressattempts
+	,compresssuccesses
+	,compressedsize < originalsize AS compression_works
+	, total_time > 0 AS takes_time 
+FROM pg_stat_toast WHERE relname = 'toast_test' ORDER BY attname;
+SELECT storagemethod,compressmethod FROM pg_stat_toast WHERE relname = 'toast_test' ORDER BY total_time DESC;
+DROP TABLE toast_test;
+SELECT count(*) FROM pg_stat_toast WHERE relname = 'toast_test';
